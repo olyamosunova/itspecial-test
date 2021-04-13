a1ca4c3b940b3ff3f7402d5f3b1e0a4c39fb4fc8
olya-mosunova <o.mosunova@skillline.ru>
2021-04-13 14:05:46 +0200

    Подключает стор

--- src/components/app/App.vue
+++ src/components/app/App.vue
@@ -3,20 +3,10 @@
 </template>
 
 <script>
+  import "./App.scss";
 
 export default {
   name: 'App',
   components: {}
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
