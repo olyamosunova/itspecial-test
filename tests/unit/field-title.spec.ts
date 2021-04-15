614bc1fb270d80fb648da867d406aaf530864cc6
olya-mosunova <o.mosunova@skillline.ru>
2021-04-15 13:01:10 +0200

    Пишет тест на смену активного типа сортировки при клике на поле

--- tests/unit/field-title.spec.ts
+++ tests/unit/field-title.spec.ts
@@ -1,4 +1,5 @@
-import { mount, createLocalVue } from "@vue/test-utils";
+import { mount, createLocalVue, shallowMount } from "@vue/test-utils";
+import Vue from "vue";
 import Vuex from "vuex";
 import FieldTitle from "@/components/field-title/Field-title.vue";
 
@@ -34,5 +35,33 @@ describe('unit tests for FieldTitle component', () => {
     it('should match the snapshot', () => {
         const wrapper = mount(FieldTitle, { store, localVue, propsData: { field } });
         expect(wrapper.html()).toMatchSnapshot();
+    });
+
+    it('change active sort type when user click sort in sortable field', () => {
+        field.sortable = true;
+
+        const wrapper = shallowMount(FieldTitle, {
+            store,
+            localVue,
+            propsData: { field }
+        });
+
+        expect((wrapper.vm as any).activeSortType).toBe('');
+        wrapper.find('th').trigger('click');
+        expect((wrapper.vm as any).activeSortType).toBe('inc');
+    });
+
+    it('not change active sort type when user click sort in not sortable field', () => {
+        field.sortable = false;
+
+        const wrapper = shallowMount(FieldTitle, {
+            store,
+            localVue,
+            propsData: { field }
+        });
+
+        expect((wrapper.vm as any).activeSortType).toBe('');
+        wrapper.find('th').trigger('click');
+        expect((wrapper.vm as any).activeSortType).toBe('');
     });
 });
