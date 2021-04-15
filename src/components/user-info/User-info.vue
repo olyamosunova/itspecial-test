f04156aca6b63f455beee45773761ea9034ff6e4
olya-mosunova <o.mosunova@skillline.ru>
2021-04-15 15:43:58 +0200

    Проверяет что данные есть

--- src/components/user-info/User-info.vue
+++ src/components/user-info/User-info.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="card mt-5">
         <div class="card-body">
-            <p class="card-text">Selected user: <b>{{user.firstName}} {{user.lastName}}</b></p>
-            <p class="card-text">Description:
+            <p class="card-text" v-if="user.firstName || user.lastName">Selected user: <b>{{user.firstName}} {{user.lastName}}</b></p>
+            <p class="card-text" v-if="user.description"> Description:
                 <textarea class="form-control" v-model="user.description" disabled></textarea>
             </p>
-            <p class="card-text">Residence address: <b>{{user.adress.streetAddress}}</b></p>
-            <p class="card-text">City: <b>{{user.adress.city}}</b></p>
-            <p class="card-text">Province / State: <b>{{user.adress.state}}</b></p>
-            <p class="card-text">Zip: <b>{{user.adress.zip}}</b></p>
+            <p class="card-text" v-if="user.adress && user.adress.streetAddress">Residence address: <b>{{user.adress.streetAddress}}</b></p>
+            <p class="card-text" v-if="user.adress && user.adress.city">City: <b>{{user.adress.city}}</b></p>
+            <p class="card-text" v-if="user.adress && user.adress.state">Province / State: <b>{{user.adress.state}}</b></p>
+            <p class="card-text" v-if="user.adress && user.adress.zip">Zip: <b>{{user.adress.zip}}</b></p>
         </div>
     </div>
 </template>
